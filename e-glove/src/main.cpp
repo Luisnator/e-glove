4624ab6e4803fa7683c63dc4c97af8fee72af59a
Phillip P <phillip.penner@fh-bielefeld.de>
2019-11-06 17:05:12 +0100

    changed example code

--- e-glove/src/main.cpp
+++ e-glove/src/main.cpp
@@ -2,157 +2,177 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-#include <BLE2902.h>
-#include <string>
+#include "BLE2902.h"
 #include "BLEHIDDevice.h"
 #include "HIDTypes.h"
 #include "HIDKeyboardTypes.h"
+#include <driver/adc.h>
+
+BLEHIDDevice* hid;
+BLECharacteristic* input;
+BLECharacteristic* output;
+
+uint8_t buttons = 0;
+uint8_t button1 = 0;
+uint8_t button2 = 0;
+uint8_t button3 = 0;
 bool connected = false;
-  BLEService *pService;
-  BLEService *pService1;
-  BLECharacteristic *reportChar1;
-  BLECharacteristic *reportChar2;
-  BLECharacteristic *reportChar3;
-  void setupCharacteristics();
-  class MyCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer){
-      connected=true;
-    }
-    
-    void onDisconnect(BLEServer* pServer){
-      connected=false;
-    }
-  };
+
+IRAM_ATTR void clickNumLock();
+IRAM_ATTR void clickCapsLock();
+IRAM_ATTR void clickScrollLock();
+
+class MyCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer){
+    connected = true;
+    BLE2902* desc = (BLE2902*)input->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+    desc->setNotifications(true);
+  }
+
+  void onDisconnect(BLEServer* pServer){
+    connected = false;
+    BLE2902* desc = (BLE2902*)input->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+    desc->setNotifications(false);
+  }
+};
+
+/*
+ * This callback is connect with output report. In keyboard output report report special keys changes, like CAPSLOCK, NUMLOCK
+ * We can add digital pins with LED to show status
+ * bit 0 - NUM LOCK
+ * bit 1 - CAPS LOCK
+ * bit 2 - SCROLL LOCK
+ */
+ class MyOutputCallbacks : public BLECharacteristicCallbacks {
+ void onWrite(BLECharacteristic* me){
+    uint8_t* value = (uint8_t*)(me->getValue().c_str());
+    ESP_LOGI(LOG_TAG, "special keys: %d", *value);
+  }
+};
+
+void taskServer(void*){
+
+
+    BLEDevice::init("ESP32-keyboard");
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyCallbacks());
+
+    hid = new BLEHIDDevice(pServer);
+    input = hid->inputReport(1); // <-- input REPORTID from report map
+    output = hid->outputReport(1); // <-- output REPORTID from report map
+
+    output->setCallbacks(new MyOutputCallbacks());
+
+    std::string name = "chegewara";
+    hid->manufacturer()->setValue(name);
+
+    hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
+    hid->hidInfo(0x00,0x02);
+
+  BLESecurity *pSecurity = new BLESecurity();
+//  pSecurity->setKeySize();
+  pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
+
+    const uint8_t report[] = {
+      USAGE_PAGE(1),      0x01,       // Generic Desktop Ctrls
+      USAGE(1),           0x06,       // Keyboard
+      COLLECTION(1),      0x01,       // Application
+      REPORT_ID(1),       0x01,        //   Report ID (1)
+      USAGE_PAGE(1),      0x07,       //   Kbrd/Keypad
+      USAGE_MINIMUM(1),   0xE0,
+      USAGE_MAXIMUM(1),   0xE7,
+      LOGICAL_MINIMUM(1), 0x00,
+      LOGICAL_MAXIMUM(1), 0x01,
+      REPORT_SIZE(1),     0x01,       //   1 byte (Modifier)
+      REPORT_COUNT(1),    0x08,
+      HIDINPUT(1),           0x02,       //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position
+      REPORT_COUNT(1),    0x01,       //   1 byte (Reserved)
+      REPORT_SIZE(1),     0x08,
+      HIDINPUT(1),           0x01,       //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
+      REPORT_COUNT(1),    0x06,       //   6 bytes (Keys)
+      REPORT_SIZE(1),     0x08,
+      LOGICAL_MINIMUM(1), 0x00,
+      LOGICAL_MAXIMUM(1), 0x65,       //   101 keys
+      USAGE_MINIMUM(1),   0x00,
+      USAGE_MAXIMUM(1),   0x65,
+      HIDINPUT(1),           0x00,       //   Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
+      REPORT_COUNT(1),    0x05,       //   5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
+      REPORT_SIZE(1),     0x01,
+      USAGE_PAGE(1),      0x08,       //   LEDs
+      USAGE_MINIMUM(1),   0x01,       //   Num Lock
+      USAGE_MAXIMUM(1),   0x05,       //   Kana
+      HIDOUTPUT(1),          0x02,       //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
+      REPORT_COUNT(1),    0x01,       //   3 bits (Padding)
+      REPORT_SIZE(1),     0x03,
+      HIDOUTPUT(1),          0x01,       //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
+      END_COLLECTION(0)
+    };
+
+    hid->reportMap((uint8_t*)report, sizeof(report));
+    hid->startServices();
+
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->setAppearance(HID_KEYBOARD);
+    pAdvertising->addServiceUUID(hid->hidService()->getUUID());
+    pAdvertising->start();
+    hid->setBatteryLevel(7);
+
+    ESP_LOGD(LOG_TAG, "Advertising started!");
+    delay(portMAX_DELAY);
   
+};
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
-  BLEDevice::init("E-Glove");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyCallbacks());
-  pService = pServer->createService((uint16_t)0x180a);
-  pService1 = pServer->createService((uint16_t)0x1812, 30);
-  setupCharacteristics();
+  pinMode(12, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(12), clickNumLock, CHANGE);   // Num Lock
+  pinMode(13, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(13), clickCapsLock, CHANGE);   // Caps Lock
+  pinMode(32, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(32), clickScrollLock, CHANGE);   // Scroll Lock
 
-  pService->start();
-  pService1->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->setAppearance(HID_KEYBOARD);
-  pAdvertising->addServiceUUID((uint16_t)0x1812);
-  pAdvertising->start();
-
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  xTaskCreate(taskServer, "server", 20000, NULL, 5, NULL);
 }
-uint8_t v[] = {0x0, 0x0, 0x0, 0x0,0x0,0x0,0x0,0x0};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(5000);
-  if(connected){
-    uint8_t a[] = {0x0, 0x0, random(0x04,0x26), 0x0,0x0,0x0,0x0,0x0};
-    reportChar1->setValue(a,sizeof(a));
-    reportChar1->notify();    
-
-    reportChar1->setValue(v, sizeof(v));
-    reportChar1->notify();
-  }
-}
-
-void setupCharacteristics() {
-
-  BLECharacteristic *manufacturer = pService->createCharacteristic(
-                                       (uint16_t)0x2a29,
-                                       BLECharacteristic::PROPERTY_READ 
-                                     );
-  std::string name = "espressif";
-  manufacturer->setValue(name);
-                                     
-  BLECharacteristic *pnpIDChar = pService->createCharacteristic(
-                                       (uint16_t)0x2a50,
-                                       BLECharacteristic::PROPERTY_READ 
-                                     );
-  const uint8_t pnp[] = {0x01,0xe5,0x02,0xcd,0xab,0x01,0x00};
-  pnpIDChar->setValue((uint8_t*)pnp, sizeof(pnp));
   
-  BLECharacteristic *hidInfoChar = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4a,
-                                       BLECharacteristic::PROPERTY_READ 
-                                     );
-  const uint8_t val1[] = {0x00,0x01,0x00,0x02};
-  hidInfoChar->setValue((uint8_t*)val1, 4);
+  if(connected){
 
-  BLECharacteristic *reportMapChar = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4b,
-                                       BLECharacteristic::PROPERTY_READ 
-                                     );
-  const uint8_t val2[] = {
-    0x05,0x01,0x09,0x06,0xA1,0x01,0x05,0x07,
-    0x19,0xE0,0x29,0xE7,0x15,0x00,0x25,0x01,
-    0x75,0x01,0x95,0x08,0x81,0x02,0x95,0x01,
-    0x75,0x08,0x81,0x01,0x95,0x05,0x75,0x01,
-    0x05,0x08,0x19,0x01,0x29,0x05,0x91,0x02,
-    0x95,0x01,0x75,0x03,0x91,0x01,0x95,0x06,
-    0x75,0x08,0x15,0x00,0x25,0x65,0x05,0x07,
-    0x19,0x00,0x29,0x65,0x81,0x00,0xC0}; //TODO
-  reportMapChar->setValue((uint8_t*)val2, sizeof(val2));
+    vTaskDelay(5000);
+    const char* hello = "Hello world from esp32 hid keyboard!!!\n";
 
-  BLECharacteristic *hidControlChar = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4c,
-                                       BLECharacteristic::PROPERTY_WRITE_NR 
-                                     );
+    while(*hello){
+      KEYMAP map = keymap[(uint8_t)*hello];
+      Serial.println(buttons);
+      uint8_t msg[] = {map.modifier || buttons, 0x0, map.usage, 0x0,0x0,0x0,0x0,0x0};
+      input->setValue(msg,sizeof(msg));
+      input->notify();
+      hello++;
+      uint8_t msg1[] = {0x0, 0x0, 0x0, 0x0,0x0,0x0,0x0,0x0};
 
-  reportChar1 = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4d,
-                                       BLECharacteristic::PROPERTY_READ   |
-                                       BLECharacteristic::PROPERTY_NOTIFY                                       
-                                     );
-  BLEDescriptor *desc1 = new BLEDescriptor(BLEUUID((uint16_t)0x2908));
-  const uint8_t desc1_val[] = {0x01, 0};
-  desc1->setValue((uint8_t*)desc1_val, 1);
-  reportChar1->addDescriptor(desc1);
-  reportChar1->addDescriptor(new BLE2902());
-
-  reportChar2 = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4d,
-                                       BLECharacteristic::PROPERTY_READ   |
-                                       BLECharacteristic::PROPERTY_WRITE                                       
-                                     );
-  BLEDescriptor *desc2 = new BLEDescriptor(BLEUUID((uint16_t)0x2908));
-  const uint8_t desc2_val[] = {0x02, 0};
-  desc2->setValue((uint8_t*)desc2_val, 1);
-  reportChar2->addDescriptor(desc2);
-
-  reportChar3 = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4d,
-                                       BLECharacteristic::PROPERTY_READ     |
-                                       BLECharacteristic::PROPERTY_WRITE    |
-                                       BLECharacteristic::PROPERTY_WRITE_NR 
-                                     );
-  BLEDescriptor *desc3 = new BLEDescriptor(BLEUUID((uint16_t)0x2908));
-  const uint8_t desc3_val[] = {0x03, 0};
-  desc3->setValue((uint8_t*)desc3_val, 1);
-  reportChar3->addDescriptor(desc3);
-
-  BLECharacteristic *protocolModeChar = pService1->createCharacteristic(
-                                       (uint16_t)0x2a4e,
-                                       BLECharacteristic::PROPERTY_WRITE_NR 
-                                     );
-
-  BLECharacteristic *bootInputChar = pService1->createCharacteristic(
-                                       (uint16_t)0x2a22,
-                                       BLECharacteristic::PROPERTY_NOTIFY
-                                     );
-  bootInputChar->addDescriptor(new BLE2902());
-
-
-  BLECharacteristic *bootOutputChar = pService1->createCharacteristic(
-                                       (uint16_t)0x2a32,
-                                       BLECharacteristic::PROPERTY_READ     |
-                                       BLECharacteristic::PROPERTY_WRITE    |
-                                       BLECharacteristic::PROPERTY_WRITE_NR 
-                                     );
-
-
-
+      input->setValue(msg1,sizeof(msg1));
+      input->notify();
+      delay(10);
+    }
+  }
+  delay(50);
 }
+
+IRAM_ATTR void clickNumLock(){
+  button1 = buttons&&0x01;
+  button1 != button1;
+  buttons = button1<<0;
+}
+IRAM_ATTR void clickCapsLock(){
+  button2 = buttons&&0x02;
+  button2 != button2;  
+  buttons = button2<<1;
+}
+IRAM_ATTR void clickScrollLock(){
+  button3 = buttons&&0x04;
+  button3 != button3;  
+  buttons = button3<<2;
+}
+
