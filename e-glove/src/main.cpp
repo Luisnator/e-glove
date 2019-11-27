5a518945cf7c21f0ec409d267cbf71f210e0587b
Phillip P <phillip.penner@fh-bielefeld.de>
2019-11-27 13:36:40 +0100

    Update main.cpp

--- e-glove/src/main.cpp
+++ e-glove/src/main.cpp
@@ -1,178 +1,111 @@
-#include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include "BLE2902.h"
-#include "BLEHIDDevice.h"
-#include "HIDTypes.h"
-#include "HIDKeyboardTypes.h"
-#include <driver/adc.h>
-
-BLEHIDDevice* hid;
-BLECharacteristic* input;
-BLECharacteristic* output;
-
-uint8_t buttons = 0;
-uint8_t button1 = 0;
-uint8_t button2 = 0;
-uint8_t button3 = 0;
-bool connected = false;
-
-IRAM_ATTR void clickNumLock();
-IRAM_ATTR void clickCapsLock();
-IRAM_ATTR void clickScrollLock();
-
-class MyCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer){
-    connected = true;
-    BLE2902* desc = (BLE2902*)input->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
-    desc->setNotifications(true);
-  }
-
-  void onDisconnect(BLEServer* pServer){
-    connected = false;
-    BLE2902* desc = (BLE2902*)input->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
-    desc->setNotifications(false);
-  }
-};
-
 /*
- * This callback is connect with output report. In keyboard output report report special keys changes, like CAPSLOCK, NUMLOCK
- * We can add digital pins with LED to show status
- * bit 0 - NUM LOCK
- * bit 1 - CAPS LOCK
- * bit 2 - SCROLL LOCK
- */
- class MyOutputCallbacks : public BLECharacteristicCallbacks {
- void onWrite(BLECharacteristic* me){
-    uint8_t* value = (uint8_t*)(me->getValue().c_str());
-    ESP_LOGI(LOG_TAG, "special keys: %d", *value);
-  }
-};
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updated by chegewara
 
-void taskServer(void*){
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
+   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   A connect hander associated with the server starts a background task that performs notification
+   every couple of seconds.
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <Arduino.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 
-    BLEDevice::init("ESP32-keyboard");
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyCallbacks());
-
-    hid = new BLEHIDDevice(pServer);
-    input = hid->inputReport(1); // <-- input REPORTID from report map
-    output = hid->outputReport(1); // <-- output REPORTID from report map
-
-    output->setCallbacks(new MyOutputCallbacks());
-
-    std::string name = "chegewara";
-    hid->manufacturer()->setValue(name);
-
-    hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
-    hid->hidInfo(0x00,0x02);
-
-  BLESecurity *pSecurity = new BLESecurity();
-//  pSecurity->setKeySize();
-  pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
-
-    const uint8_t report[] = {
-      USAGE_PAGE(1),      0x01,       // Generic Desktop Ctrls
-      USAGE(1),           0x06,       // Keyboard
-      COLLECTION(1),      0x01,       // Application
-      REPORT_ID(1),       0x01,        //   Report ID (1)
-      USAGE_PAGE(1),      0x07,       //   Kbrd/Keypad
-      USAGE_MINIMUM(1),   0xE0,
-      USAGE_MAXIMUM(1),   0xE7,
-      LOGICAL_MINIMUM(1), 0x00,
-      LOGICAL_MAXIMUM(1), 0x01,
-      REPORT_SIZE(1),     0x01,       //   1 byte (Modifier)
-      REPORT_COUNT(1),    0x08,
-      HIDINPUT(1),           0x02,       //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position
-      REPORT_COUNT(1),    0x01,       //   1 byte (Reserved)
-      REPORT_SIZE(1),     0x08,
-      HIDINPUT(1),           0x01,       //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
-      REPORT_COUNT(1),    0x06,       //   6 bytes (Keys)
-      REPORT_SIZE(1),     0x08,
-      LOGICAL_MINIMUM(1), 0x00,
-      LOGICAL_MAXIMUM(1), 0x65,       //   101 keys
-      USAGE_MINIMUM(1),   0x00,
-      USAGE_MAXIMUM(1),   0x65,
-      HIDINPUT(1),           0x00,       //   Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position
-      REPORT_COUNT(1),    0x05,       //   5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
-      REPORT_SIZE(1),     0x01,
-      USAGE_PAGE(1),      0x08,       //   LEDs
-      USAGE_MINIMUM(1),   0x01,       //   Num Lock
-      USAGE_MAXIMUM(1),   0x05,       //   Kana
-      HIDOUTPUT(1),          0x02,       //   Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
-      REPORT_COUNT(1),    0x01,       //   3 bits (Padding)
-      REPORT_SIZE(1),     0x03,
-      HIDOUTPUT(1),          0x01,       //   Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile
-      END_COLLECTION(0)
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
     };
 
-    hid->reportMap((uint8_t*)report, sizeof(report));
-    hid->startServices();
-
-    BLEAdvertising *pAdvertising = pServer->getAdvertising();
-    pAdvertising->setAppearance(HID_KEYBOARD);
-    pAdvertising->addServiceUUID(hid->hidService()->getUUID());
-    pAdvertising->start();
-    hid->setBatteryLevel(7);
-
-    ESP_LOGD(LOG_TAG, "Advertising started!");
-    delay(portMAX_DELAY);
-  
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
 };
+
+
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
 
-  pinMode(12, INPUT_PULLDOWN);
-  attachInterrupt(digitalPinToInterrupt(12), clickNumLock, CHANGE);   // Num Lock
-  pinMode(13, INPUT_PULLDOWN);
-  attachInterrupt(digitalPinToInterrupt(13), clickCapsLock, CHANGE);   // Caps Lock
-  pinMode(32, INPUT_PULLDOWN);
-  attachInterrupt(digitalPinToInterrupt(32), clickScrollLock, CHANGE);   // Scroll Lock
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
 
-  xTaskCreate(taskServer, "server", 20000, NULL, 5, NULL);
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  
-  if(connected){
-
-    vTaskDelay(5000);
-    const char* hello = "Hello world from esp32 hid keyboard!!!\n";
-
-    while(*hello){
-      KEYMAP map = keymap[(uint8_t)*hello];
-      Serial.println(buttons);
-      uint8_t msg[] = {map.modifier || buttons, 0x0, map.usage, 0x0,0x0,0x0,0x0,0x0};
-      input->setValue(msg,sizeof(msg));
-      input->notify();
-      hello++;
-      uint8_t msg1[] = {0x0, 0x0, 0x0, 0x0,0x0,0x0,0x0,0x0};
-
-      input->setValue(msg1,sizeof(msg1));
-      input->notify();
-      delay(10);
+    // notify changed value
+    if (deviceConnected) {
+        pCharacteristic->setValue((uint8_t*)&value, 4);
+        pCharacteristic->notify();
+        value++;
+        delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
     }
-  }
-  delay(50);
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
-
-IRAM_ATTR void clickNumLock(){
-  button1 = buttons&&0x01;
-  button1 != button1;
-  buttons = button1<<0;
-}
-IRAM_ATTR void clickCapsLock(){
-  button2 = buttons&&0x02;
-  button2 != button2;  
-  buttons = button2<<1;
-}
-IRAM_ATTR void clickScrollLock(){
-  button3 = buttons&&0x04;
-  button3 != button3;  
-  buttons = button3<<2;
-}
-
